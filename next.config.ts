9121ad973519cf1e4cf45d6cafaedc7b6c560c17
marcosgratacos@gmail.com <marcosgratacos@gmail.com>
2025-10-15 21:30:17 +0200

    Configurar Next.js para permitir imÃ¡genes externas de Steam

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.akamai.steamstatic.com',
+        port: '',
+        pathname: '/steam/apps/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
